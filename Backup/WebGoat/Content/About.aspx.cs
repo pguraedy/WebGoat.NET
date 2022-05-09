61884266071d18b9f4fd2851af8dae62e5911515
pguraedy <philip@guraedy.com>
2022-05-09 10:57:42 -0500

    .

---
+++ Backup/WebGoat/Content/About.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OWASP.WebGoat.NET
+{
+    public partial class About : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
